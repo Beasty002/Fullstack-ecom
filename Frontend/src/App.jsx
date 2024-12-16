db7950bed696215d142eca8f79de302c49bb4bbe
Beasty002 <xasbin12@gmail.com>
2024-12-16 20:50:27 +0545

    ft : search bar show hide

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,11 +10,13 @@ import Orders from './pages/Orders'
 import PlaceOrder from './pages/PlaceOrder'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import SearchBar from './components/searchBar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <NavBar />
+      <SearchBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
