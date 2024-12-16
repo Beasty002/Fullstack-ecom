60dc7aed1cde1873231951fd39ae8776670b0f82
Beasty002 <xasbin12@gmail.com>
2024-12-16 15:28:57 +0545

    ft : sort

--- Frontend/src/pages/Collection.jsx
+++ Frontend/src/pages/Collection.jsx
@@ -12,6 +12,7 @@ const Collection = () => {
     const [filterProducts, setFilterProducts] = useState([]);
     const [category, setCategory] = useState([]);
     const [subCategory, setSubCategory] = useState([]);
+    const [sortType, setSortType] = useState('relevant')
 
     const toggleCategory = (e) => {
         if (category.includes(e.target.value)) {
@@ -33,6 +34,7 @@ const Collection = () => {
 
     const applyFilter = () => {
         let productsCopy = products.slice();
+        //slice use garnu ko reason chai copy banauna ho products ko directly equal gare main product ma change hunthyo products copy ma change vaye
         if (category.length > 0) {
             productsCopy = productsCopy.filter(item => category.includes(item.category))
         }
@@ -42,11 +44,29 @@ const Collection = () => {
         setFilterProducts(productsCopy)
     }
 
+    const sortProduct = () => {
+        let fpCopy = filterProducts.slice();
+        switch (sortType) {
+            case 'low-high':
+                setFilterProducts(fpCopy.sort((a, b) => (a.price - b.price)));
+                break;
+            case 'high-low':
+                setFilterProducts(fpCopy.sort((a, b) => (b.price - a.price)));
+                break;
+            default:
+                applyFilter();
+                break;
+        }
+    }
+
 
 
     useEffect(() => {
         applyFilter();
     }, [category, subCategory])
+    useEffect(() => {
+        sortProduct();
+    }, [sortType])
 
 
 
@@ -104,7 +124,7 @@ const Collection = () => {
                 <div className='flex justify-between text-base sm:text-2xl mb-4'>
                     <Title text1={'ALL'} text2={'COLLECTIONS'} />
                     {/* Product Sort */}
-                    <select className='border-2 border-gray-300 text-sm px-2'>
+                    <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2' >
                         <option value="relevant">Sort by : Relevant</option>
                         <option value="low-high">Sort by: Low to High</option>
                         <option value="high-low">Sort by: High to Low</option>
