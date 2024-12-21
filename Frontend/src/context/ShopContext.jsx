8276b968f18dd9a1cbe74377311ce3156f8b78ba
Beasty002 <xasbin12@gmail.com>
2024-12-21 19:21:51 +0545

    ft : cart page done

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -49,13 +49,40 @@ const ShopContextProvider = (props) => {
     }
 
 
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = Number(quantity)
+        setCartitems(cartData);
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items);
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItems[items][item]
+                    }
+                }
+                catch (error) {
+
+                }
+            }
+        }
+        return totalAmount;
+
+    }
+
+
 
 
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
-        getCartCount
+        getCartCount, updateQuantity,
+        getCartAmount
     }
     return (
         <ShopContext.Provider value={value}>
