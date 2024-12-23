92bd22413bea032fa6045462f4ef4adc3f872664
Beasty002 <xasbin12@gmail.com>
2024-12-23 22:23:37 +0545

    ft: backend models and routes controllers

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,17 @@
+//Route for user login
+const loginUser = async (req, res) => {
+
+}
+
+//Route for user regirstration
+
+const registerUser = async (req, res) => {
+    res.json({ msg: "Register api working" })
+}
+
+//Route for admin login
+const adminLogin = async (req, res) => {
+
+}
+
+export { loginUser, registerUser, adminLogin }
