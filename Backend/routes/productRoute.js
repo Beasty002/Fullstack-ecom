c398f839e0424e857282d56cac2e1a4d5aad24d5
Beasty002 <xasbin12@gmail.com>
2024-12-27 15:09:54 +0545

    ft : admin authentication for product add and remove

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { listProucts, addProduct, removeProduct, singleProduct } from '../controllers/productController.js'
 import upload from '../middleware/multer.js';
+import adminAuth from '../middleware/adminAuth.js';
 
 const productRouter = express.Router();
-productRouter.post('/add', upload.fields([{ name: 'image1', maxCount: 1 }, { name: 'image2', maxCount: 1 }, { name: 'image3', maxCount: 1 }, { name: 'image4', maxCount: 1 }]), addProduct);
+productRouter.post('/add', adminAuth, upload.fields([{ name: 'image1', maxCount: 1 }, { name: 'image2', maxCount: 1 }, { name: 'image3', maxCount: 1 }, { name: 'image4', maxCount: 1 }]), addProduct);
 productRouter.post('/single', singleProduct);
 productRouter.get('/list', listProucts);
-productRouter.post('/remove', removeProduct);
+productRouter.post('/remove', adminAuth, removeProduct);
 
 export default productRouter
